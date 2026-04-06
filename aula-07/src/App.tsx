8e51d2375f7199cef480a41e064a20b7bdce2952
if-Kaliel <kaliel.aquino1303@gmail.com>
2026-04-06 11:56:02 -0300

    Estrutura de projeto criada.

---
+++ aula-07/src/App.tsx
@@ -0,0 +1,13 @@
+import Cabecalho from "./components/cabecalho/Cabecalho"
+import Rodape from "./components/rodape/Rodape"
+import Main from "./components/main/Main"
+
+export default function App() {
+  return(
+    <div>
+   <Cabecalho/>
+   <Main/>
+   <Rodape/>
+  </div>
+)}
+ 
