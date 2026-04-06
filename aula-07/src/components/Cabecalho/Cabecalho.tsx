c95a9d254dbcf4e582c176a95a804f740b9dcefe
GuilhermeAnitelli <guilherme.anitelli10@gmail.com>
2026-04-06 12:12:47 -0300

    feat: Iniciando o TypeScrit com React e Vite e TailwindCSS.

---
+++ aula-07/src/components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,23 @@
+
+
+export default function Cabecalho() {
+
+    return(
+    <header className="bg-[#ff0000] w-[80%] mx-auto">
+      <h1></h1>
+      <nav>
+        <ul className="menu">
+          <li>
+            <a href="#">Item-1</a>
+          </li>
+          <li>
+            <a href="#">Item-2</a>
+          </li>
+          <li>
+            <a href="#">Item-3</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    )
+}
---
+++ aula-07/src/components/Main/Main.tsx
@@ -0,0 +1,42 @@
+
+
+export default function Main() {
+    return(
+        <main>
+      <section>
+        <h2>Item 1</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, optio
+          debitis animi natus voluptate repellendus! Temporibus totam laboriosam
+          quibusdam harum provident vitae consectetur, ad alias eaque aperiam
+          culpa porro nobis at nulla voluptatum nihil aliquid. Obcaecati dolorum
+          ex eius similique nobis molestiae aperiam deserunt aut error.
+          Blanditiis accusamus porro laudantium!
+        </p>
+      </section>
+      <section>
+        <h2>Item 2</h2>
+        <p>
+          Natus repellat maiores nam impedit aliquam suscipit quos a officia
+          sunt amet. Illum, consequuntur. Non doloribus corrupti nisi
+          exercitationem voluptatibus cupiditate eos consequuntur accusantium at
+          cumque nam soluta omnis ad, autem minus vitae ut architecto
+          consectetur iusto! Neque saepe molestiae cum voluptatum doloribus,
+          facilis culpa minus, quidem porro nostrum repellendus.
+        </p>
+      </section>
+      <section>
+        <h2>Item 3</h2>
+        <p>
+          Laborum assumenda labore facilis fugit corporis praesentium ducimus
+          repellendus numquam molestias sed error deleniti sint autem atque,
+          exercitationem iure ipsum dolorum soluta mollitia distinctio.
+          Praesentium facere blanditiis laborum aliquid nesciunt labore sed,
+          quidem, expedita, deleniti nobis adipisci. Omnis, debitis. Dolore
+          maiores voluptatem enim cum excepturi suscipit, deleniti dolorum
+          veritatis officiis!
+        </p>
+      </section>
+    </main>
+    )
+}
