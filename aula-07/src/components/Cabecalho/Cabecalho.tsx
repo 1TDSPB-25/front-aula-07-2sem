b6181ff6655b05135ba65c4299f54c7d676997f1
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2026-04-06 12:34:00 -0300

    feat : Exemplo de modularização + instalação e uso do tailwind.

---
+++ aula-07/src/components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,17 @@
+
+
+export default function Cabecalho(){
+
+    return(
+        <header className="bg-[#904848] w-[80%] mx-auto">
+        <h1>Logo</h1>
+        <nav>
+          <ul className="menu"> 
+            <li><a href="#">Item-1</a></li>
+            <li><a href="#">Item-2</a></li>
+            <li><a href="#">Item-3</a></li>
+          </ul>
+        </nav>
+      </header>
+    );
+}
---
+++ aula-07/src/components/Main/Main.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Main() {
+  return (
+    <main>
+        <section>
+          <h2>Item 1</h2>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate at doloremque repellat voluptates mollitia in saepe sint accusamus, eligendi facilis eum architecto aperiam tempore qui facere ipsum laborum aut eos officiis perferendis. Quos maxime cum tenetur nisi fugiat cumque accusantium, ratione quo. Voluptates distinctio quod iste debitis. Expedita, assumenda adipisci.</p>
+        </section>
+        <section>
+          <h2>Item 2</h2>
+          <p>Ipsam eos velit magnam porro id corporis perferendis, ipsum, illo blanditiis repellat explicabo! Molestiae eius non doloribus consequatur vel nemo vitae voluptatem iste unde dicta. Modi provident voluptas quod natus vero architecto nulla nesciunt magnam, aspernatur odio pariatur hic minus sequi tempore voluptatem porro dolore quibusdam deserunt totam, in rerum.</p>
+        </section>
+        <section>
+          <h2>Item 3</h2>
+          <p>Neque minus labore maxime deserunt. Dolores labore eius temporibus praesentium alias fugiat molestiae eligendi itaque expedita in, libero maxime, a quisquam, minus iusto commodi. Aperiam, perferendis omnis dolore optio vero quasi deserunt adipisci! Veniam praesentium earum libero officiis facere corrupti, totam iusto itaque harum perferendis voluptatibus placeat, ducimus aperiam quis.</p>
+        </section>
+      </main>
+  )
+}
